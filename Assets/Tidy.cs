d3ee2c6ec3a5ded70597ab75719c3978a4264008
Jacky Lee <dr.jackylee@gmail.com>
2017-09-16 13:18:42 -0700

    Initial commit

---
+++ Assets/Tidy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tidy : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter (Collision collision) {
+        Destroy(gameObject);
+	}
+}
