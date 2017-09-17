27e3a0e2904aa50c399d720f90f73e2e6a7d3493
CS70 Cobra <ogillespie@cs.hmc.edu>
2017-09-16 20:48:12 -0700

    Added holotoolkit

---
+++ Assets/SpawnBall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpawnBall : MonoBehaviour
+{
+
+    public Rigidbody projectile;
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    // Use this for initialization
+    void Start()
+    {
+        keywords.Add("Spawn ball", () =>
+        {
+            print("Spawn ball");
+            Instantiate(projectile, transform.position, Quaternion.identity);
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
---
+++ Assets/initBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class initBall : MonoBehaviour {
+    public float initialForce;
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(0, 0, initialForce);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
