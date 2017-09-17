bdf7bcdc934f16e77830268305a2323ce881377c
Jacky Lee <dr.jackylee@gmail.com>
2017-09-17 05:00:37 -0700

    Add score tracking scripts

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;        // The player's score.
+
+
+    Text text;                      // Reference to the Text component.
+
+
+    //void Awake()
+    void Start()
+    {
+        // Set up the reference.
+        //text = gameObject.GetComponent<Text>();
+        ////text.text = "";
+        // Reset the score.
+        score = 0;
+    }
+
+
+    void Update()
+    {
+        // Set the displayed text to be the word "Score" followed by the score value.
+        //text.text = "Score: " + (score / 4);
+    }
+
+    public void updateScore()
+    {
+        score++;
+    }
+}
---
+++ Assets/ScoreObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box1 : MonoBehaviour {
+
+    public ScoreManager scoreManager;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        scoreManager.updateScore();
+    }
+}
